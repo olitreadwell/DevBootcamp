eb61aad2b91edc543d98e53e5a6247834f05bdd7
Oli Treadwell <olitreadwell@gmail.com>
2014-01-05 18:09:33 -0800

    5th Commit Jan 5 2014 1809

---
+++ defining_a_hash.rb
@@ -0,0 +1,21 @@
+=begin
+Exercise: Defining a Hash
+Define a Hash variable called my_info which has the following keys:
+
+:first_name
+:last_name
+:hometown
+:age
+Use your own personal information to fill in the keys. Make sure :age is a positive integer and the rest are non-empty Strings.
+
+The Hash needs to be stored in the my_info variable.
+=end
+
+# my_info is empty to start.  Edit this!
+
+my_info = {
+:first_name => "Oliver",
+:last_name => "Treadwell", 
+:hometown => "San Diego, CA", 
+:age => "24"
+}
